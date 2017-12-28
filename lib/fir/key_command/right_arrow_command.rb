a7c3a6423da8124d5d9480fb060c55af038da504
Dean Nasseri <dnasseri10@gmail.com>
2017-12-28 11:55:41 -0500

    implement right arrow command

---
+++ lib/fir/key_command/right_arrow_command.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+# encoding: UTF-8
+
+require_relative './key_command'
+
+class Fir
+  class RightArrowCommand < KeyCommand
+    def self.character_regex
+      /^\e\[C$/
+    end
+
+    def execute_hook(new_state)
+      unless state.blank?
+        if state.cursor.x < state.lines[state.cursor.y].length
+          new_state.cursor = state.cursor.right(1)
+        end
+      end
+      new_state
+    end
+  end
+end
